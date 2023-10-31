c7315f847fef5c39db898e36bd3a4bb51054883f
ezbw69 <lipfaris00@gmail.com>
2023-10-31 11:50:36 +0700

    Update userbot.py
    Signed-off-by: ezbw69 <lipfaris00@gmail.com>

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.one.join_chat("hero4in")
+                await self.one.join_chat("worstpartyinc")
             except:
                 pass
             assistants.append(1)
